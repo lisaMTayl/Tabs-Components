eb8299ca2c3902392df21ac1da9ae263fcb5cf87
Lisa <lmkirk@gmail.com>
2019-06-20 17:24:57 -0400

    this.tabItem is not a function error

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,46 +3,50 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
+    this.element = element;
 
     // Get the custom data attribute on the Link
     // this.data;
-
+    this.data = this.element.classList.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tab-links[data-tab='${this.element.dataset.tab}']`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () =>this.tabItem());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    
+
     // Call the select method on the item associated with this link
 
   }
 }
 
 class TabItem {
-  constructor(element) {
+  constructor(item) {
     // Assign this.element to the passed in element
     // this.element;
+    this.item = item;
 
   }
 
   select() {
+
     // Select all ".tabs-item" elements from the DOM
     // const items;
-
+    const items = items;
+    items = document.querySelectorAll()
     // Remove the class "tabs-item-selected" from each element
     
     // Add a class named "tabs-item-selected" to this element
