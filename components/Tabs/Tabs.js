cdc42cbbe5d5883eadd744e754014336b012817f
Lisa <lmkirk@gmail.com>
2019-06-20 17:42:37 -0400

    lost on tabItem not a function

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -7,13 +7,13 @@ class TabLink {
 
     // Get the custom data attribute on the Link
     // this.data;
-    this.data = this.element.classList.tab;
+    this.data = this.element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.itemElement = document.querySelector(`.tab-links[data-tab='${this.element.dataset.tab}']`);
+    this.itemElement = document.querySelector(`.tab-items[data-tab='${this.data}']`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    this.tabItem = new TabItem(this.itemElement);
+    this.tabItem = new TabItem(this.tabItem);
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click', () =>this.tabItem());
   };
@@ -24,7 +24,7 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-
+    Array.from(links).forEach();
     // Add a class named "tabs-link-selected" to this link
     // this.element;
 
